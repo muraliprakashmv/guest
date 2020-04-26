60aaa15458b5f0e8ce10763607efe48e64d69bd6
mvmurali <mvmuraliprakash@hotmail.com>
2020-04-26 23:23:08 +0530

    login page c3

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,render_to_response
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from webapp.forms import loginForm,studentForm
 from django.contrib.auth.decorators import login_required
 from webapp.models import student
+from django.template import RequestContext
 
 
 # Create your views here.
@@ -125,3 +126,12 @@ def Signout(request):
     for k in request.session.keys():
         del request.session[k]
     return redirect('Signin')
+
+
+
+# 404 Page Not found
+def handler404(request):
+    response = render_to_response('404.html', {},
+                              context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
