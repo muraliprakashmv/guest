12ea0e9c702a181c471e79686217a1e2c42adf0d
mvmurali <mvmuraliprakash@hotmail.com>
2020-04-27 07:04:28 +0530

    static path

--- myapp/settings.py
+++ myapp/settings.py
@@ -135,5 +135,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),
-                        '~/gms_django/guest/static/',
+                        '/root/gms_django/guest/static',
                                  )
